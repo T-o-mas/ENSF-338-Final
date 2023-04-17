a6a592f8a5e10a838b45d714498cd15e68dc7dbb
BrianUCalgary <114112708+BrianUCalgary@users.noreply.github.com>
2023-04-16 19:22:50 -0600

    added sort to queue and stack, moved to correct folder

---
+++ myLib/datastructures/nodes/DNode.java
@@ -0,0 +1,38 @@
+package myLib.datastructures.nodes;
+
+public class DNode {
+    private DNode prev;
+    private DNode next;
+    private int data;
+    
+    public DNode() {
+    }
+
+    public DNode(int toStore) {
+        this.data = toStore;
+    }
+
+    public DNode getPrevNode() {
+        return this.prev;
+    }
+
+    public DNode getNextNode() {
+        return this.next;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setPrevNode(DNode newNode) {
+        this.prev = newNode;
+    }
+
+    public void setNextNode(DNode newNode) {
+        this.next = newNode;
+    }
+
+    public void setData(int newData) {
+        this.data = newData;
+    }
+}
